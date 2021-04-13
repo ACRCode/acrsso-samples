87ac8d5d1f9aedc8d81cf659baee30863bf89440
Sergei Rusinov <rsage@mail.ru>
2021-04-13 16:06:22 -0400

    WPF Auth Code flow sample

---
+++ src/AcrSso.WpfSample/MainViewModel.cs
@@ -0,0 +1,134 @@
+﻿using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Browser;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AcrSso.WpfSample
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _authority = "https://sso.acr.org/oauth2/default";
+        private string _additionalEndpoints = "https://sso.acr.org/oauth2/v1/";
+        private string _clientId = "[Client ID]";
+        private string _clientSecret = "[Client Secret]";
+        private string _redirectUrl = "[Redirect URL]";
+        private string _scopes = "openid offline_access lcsr_data_submission";
+        private string _accessToken;
+        private string _idToken;
+        private string _refreshToken;
+        private OidcClient _oidcClient;
+
+        public MainViewModel()
+        {
+            var options = new OidcClientOptions
+            {
+                Authority = Authority,
+                ClientId = ClientId,
+                ClientSecret = ClientSecret,
+                Scope = Scopes,
+                RedirectUri = RedirectUrl,
+                Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode,
+                ResponseMode = OidcClientOptions.AuthorizeResponseMode.Redirect,
+            };
+
+            options.Policy.Discovery.AdditionalEndpointBaseAddresses.Add(AdditionalEndpoints);
+
+            _oidcClient = new OidcClient(options);
+        }
+
+        public string Authority
+        {
+            get => _authority;
+            set => SetProperty(ref _authority, value);
+        }
+
+        public string AdditionalEndpoints
+        {
+            get => _additionalEndpoints;
+            set => SetProperty(ref _additionalEndpoints, value);
+        }
+        
+        public string ClientId
+        {
+            get => _clientId;
+            set => SetProperty(ref _clientId, value);
+        }
+
+        public string ClientSecret
+        {
+            get => _clientSecret;
+            set => SetProperty(ref _clientSecret, value);
+        }
+
+        public string RedirectUrl
+        {
+            get => _redirectUrl;
+            set => SetProperty(ref _redirectUrl, value);
+        }
+
+        public string Scopes
+        {
+            get => _scopes;
+            set => SetProperty(ref _scopes, value);
+        }
+        public string AccessToken
+        {
+            get => _accessToken;
+            set => SetProperty(ref _accessToken, value);
+        }
+
+        public string IdToken
+        {
+            get => _idToken;
+            set => SetProperty(ref _idToken, value);
+        }
+
+        public string RefreshToken
+        {
+            get => _refreshToken;
+            set => SetProperty(ref _refreshToken, value);
+        }
+
+        public object MessageBoxButtons { get; private set; }
+
+        public async Task Login(IBrowser browser)
+        {
+            _oidcClient.Options.Authority    = Authority;
+            _oidcClient.Options.ClientId     = ClientId;
+            _oidcClient.Options.ClientSecret = ClientSecret;
+            _oidcClient.Options.Scope        = Scopes;
+            _oidcClient.Options.RedirectUri  = RedirectUrl;
+            _oidcClient.Options.Browser      = browser;
+
+            var result = await _oidcClient.LoginAsync(new LoginRequest { BrowserDisplayMode = DisplayMode.Visible});
+
+            if (result.IsError)
+            {
+                MessageBox.Show(result.Error, "Login", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            AccessToken  = result.AccessToken;
+            IdToken      = result.IdentityToken;
+            RefreshToken = result.RefreshToken;
+        }
+
+        #region INotifyPropertyChanged Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SetProperty<T>(ref T prop, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if ((prop == null && newValue == null) || (prop != null && prop.Equals(newValue))) return;
+
+            prop = newValue;
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion INotifyPropertyChanged Implementation
+    }
+}
---
+++ src/AcrSso.WpfSample/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Browser;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AcrSso.WpfSample
+{
+    public partial class MainWindow : Window, IBrowser
+    {
+        private MainViewModel _mainViewModel;
+        private BrowserResult _result;
+        private readonly SemaphoreSlim _signal = new SemaphoreSlim(0, 1);
+        private BrowserOptions _options;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mainViewModel = new MainViewModel();
+            DataContext = _mainViewModel;
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            await _mainViewModel.Login(this);
+        }
+
+        private void SsoWebBrowser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_options.EndUrl) && e.Uri.OriginalString.StartsWith(_options.EndUrl) && _options.ResponseMode == OidcClientOptions.AuthorizeResponseMode.Redirect)
+            {
+                _result.ResultType = BrowserResultType.Success;
+                _result.Response = e.Uri.OriginalString;
+                _signal.Release();
+            }
+        }
+
+        private void SsoWebBrowser_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_options.EndUrl) && e.Uri.OriginalString.StartsWith(_options.EndUrl))
+            {
+                e.Cancel = true;
+                _result.ResultType = BrowserResultType.Success;
+                if (_options.ResponseMode == OidcClientOptions.AuthorizeResponseMode.Redirect)
+                {
+                    _result.Response = e.Uri.OriginalString;
+                }
+                _signal.Release();
+            }
+        }
+
+        public async Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = default)
+        {
+            _options = options;
+            _result = new BrowserResult
+            {
+                ResultType = BrowserResultType.UserCancel
+            };
+
+            SsoWebBrowser.Navigate(options.StartUrl);
+            await _signal.WaitAsync();
+
+            return _result;
+        }
+    }
+}
