beb2d6d6ecae2c06a00636cc8409ff3817360d50
Sergei Rusinov <rsage@mail.ru>
2021-04-14 01:36:27 -0400

    MVC Sample: Login with ACR SSO and display the tokens

---
+++ src/AcrSso.MvcSample/Controllers/AcrSsoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcrSso.MvcSample.Controllers
+{
+    public class AcrSsoController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            ViewBag.Message = "This is a sample page that demonstrates how to initiate login process with ACR SSO.";
+
+            var authResult = await HttpContext.GetOwinContext().Authentication.AuthenticateAsync(OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            if(authResult != null)
+            {
+                ViewBag.AccessToken = authResult.Properties.Dictionary["access_token"];
+                ViewBag.IdToken = authResult.Properties.Dictionary["id_token"];
+                ViewBag.RefreshToken = authResult.Properties.Dictionary["refresh_token"];
+            }
+
+            return View();
+        }
+
+        public void SignIn()
+        {
+            HttpContext
+                .GetOwinContext()
+                .Authentication
+                .Challenge(new AuthenticationProperties { RedirectUri = "AcrSso" }, OpenIdConnectAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
---
+++ src/AcrSso.MvcSample/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Owin.Extensions;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace AcrSso.MvcSample
+{
+    public class Startup
+    {
+        private static readonly string Authority = ConfigurationManager.AppSettings["Authority"];
+        private static readonly string ClientId = ConfigurationManager.AppSettings["ClientId"];
+        private static readonly string ClientSecret = ConfigurationManager.AppSettings["ClientSecret"];
+        private static readonly string Scopes = ConfigurationManager.AppSettings["Scopes"];
+        private static readonly string RedirectUrl = ConfigurationManager.AppSettings["RedirectUrl"];
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(OpenIdConnectAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions() { AuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType });
+
+            var oidcOptions = new OpenIdConnectAuthenticationOptions(OpenIdConnectAuthenticationDefaults.AuthenticationType)
+            {
+                SignInAsAuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                Authority = Authority,
+                ClientId = ClientId,
+                ClientSecret = ClientSecret,
+                ResponseType = "code",
+                Scope = Scopes,
+                RedirectUri = RedirectUrl,
+                RedeemCode = true,
+                SaveTokens = true, // this is only needed for demonstration purpose to make sure the tokens are stored in the cookies and as awailable on the AcrSso page to display.
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    TokenResponseReceived = OnTokenResponseReceived
+                }
+            };
+            app.UseOpenIdConnectAuthentication(oidcOptions);
+
+            // This makes any middleware defined above this line run before the Authorization rule is applied in web.config
+            app.UseStageMarker(PipelineStage.Authenticate);
+        }
+
+        private async Task OnTokenResponseReceived(TokenResponseReceivedNotification arg)
+        {
+            //Put you code that stores to tokens to future use
+            var accessToken = arg.TokenEndpointResponse.AccessToken;
+            var refreshToken = arg.TokenEndpointResponse.RefreshToken;
+        }
+    }
+}
