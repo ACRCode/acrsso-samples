68d3f3c350ae7d8f228788b23b9fc612ea51ec1b
Sergei Rusinov <rsage@mail.ru>
2021-04-14 00:53:04 -0400

    WebForms Sample: Login with ACR SSO and display the tokens

--- src/AcrSso.WebFormsSample/AcrSso.aspx
+++ src/AcrSso.WebFormsSample/AcrSso.aspx
@@ -2,6 +2,17 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <h2><%: Title %>.</h2>
-    <h3>Your application description page.</h3>
-    <p>Use this area to provide additional information.</p>
+    <h3>This is a sample page that demonstrates how to initiate login process with ACR SSO.</h3>
+    <br />
+    <h4>Click <asp:LinkButton Text="Sign in" runat="server" OnClick="SignIn_Click" /> to login with ACR SSO.</h4>
+    <br />
+    <br />
+    <asp:Panel ID="TokensPanel" runat="server">
+        <h4>Access Token:</h4>
+        <asp:Label ID="AccessToken" runat="server" />
+        <h4>ID Token:</h4>
+        <asp:Label ID="IdToken" runat="server" />
+        <h4>Refresh Token:</h4>
+        <asp:Label ID="RefreshToken" runat="server" />
+    </asp:Panel>
 </asp:Content>
--- src/AcrSso.WebFormsSample/AcrSso.aspx.cs
+++ src/AcrSso.WebFormsSample/AcrSso.aspx.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace AcrSso.WebFormsSample
 {
@@ -11,7 +10,27 @@ namespace AcrSso.WebFormsSample
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var authWaiter = HttpContext.Current.GetOwinContext().Authentication.AuthenticateAsync(OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            authWaiter.Wait();
+            var authResult = authWaiter.Result;
+            if (authResult != null)
+            {
+                AccessToken.Text  = authResult.Properties.Dictionary["access_token"];
+                IdToken.Text      = authResult.Properties.Dictionary["id_token"];
+                RefreshToken.Text = authResult.Properties.Dictionary["refresh_token"];
+                TokensPanel.Visible = true;
+            }
+            else
+            {
+                TokensPanel.Visible = false;
+            }
+        }
 
+        protected void SignIn_Click(object sender, EventArgs e)
+        {
+            HttpContext.Current.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "AcrSso" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
         }
     }
 }
